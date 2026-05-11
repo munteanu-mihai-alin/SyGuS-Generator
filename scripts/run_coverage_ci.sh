a4a642cedfd912a9d5468928af455b28d6579347
Mihai M <munteanu.mihaialin97@gmail.com>
2026-05-11 05:09:10 +0300

    fix coverage branch data

--- scripts/run_coverage_ci.sh
+++ scripts/run_coverage_ci.sh
@@ -43,12 +43,14 @@ cmake "${cmake_args[@]}"
 cmake --build "${BUILD_DIR}" -j"$(nproc)"
 ctest --test-dir "${BUILD_DIR}" --output-on-failure
 
-lcov --capture --directory "${BUILD_DIR}" --output-file "${BUILD_DIR}/coverage.info"
+LCOV_OPTS=(--rc branch_coverage=0 --ignore-errors mismatch,empty,negative)
+
+lcov --capture --directory "${BUILD_DIR}" --output-file "${BUILD_DIR}/coverage.info" "${LCOV_OPTS[@]}"
 lcov --remove "${BUILD_DIR}/coverage.info" \
   '/usr/*' \
   '*/tests/*' \
   '*/build-coverage/*' \
-  --output-file "${BUILD_DIR}/coverage.filtered.info"
-genhtml "${BUILD_DIR}/coverage.filtered.info" --output-directory "${BUILD_DIR}/coverage-html"
+  --output-file "${BUILD_DIR}/coverage.filtered.info" "${LCOV_OPTS[@]}"
+genhtml "${BUILD_DIR}/coverage.filtered.info" --output-directory "${BUILD_DIR}/coverage-html" "${LCOV_OPTS[@]}"
 
 echo "Coverage report generated under ${BUILD_DIR}/coverage-html"
